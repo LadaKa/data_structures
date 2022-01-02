a1f95ce98c77178f51440b573e9a5ab0b0ab10ad
LadaKa <LadaKudlackova@seznam.cz>
2022-01-02 19:40:07 +0100

    Removed parent pointer. Modified keys: unordered_map.

--- find_duplicates.h
+++ find_duplicates.h
@@ -5,37 +5,22 @@
 class node {
 public:
 	
-	vector<node *> children;
-	vector<char> keys;
-	node *parent;
-	int defaultKeysSize = 30; // TODO - dyn. resize
+	unordered_map<char, node*> children;
 
-	// Constructor: initialize a node with one key
+	// Constructor: initialize an empty node
 	node()
 	{
-		keys.reserve(defaultKeysSize);
-		children.reserve(defaultKeysSize);
 	}
 
-	// If this node contains the given key, return true and set i to key's position.
-	// Otherwise return false and set i to the first key greater than the given one.
-	bool find_branch(char key, int &i)
+	bool has_key(char key)
 	{
-		i = 0;
-		while (i < keys.size() && keys[i] <= key) {
-			if (keys[i] == key)
-				return true;
-			i++;
-		}
-		return false;
+		return children.find(key) != children.end();
 	}
 
-	// Insert a new key and a new child at position i.
-	void insert_node(int i, char key, node *child)
+	// Insert a new key and a new child
+	void insert_node(char key, node *child)
 	{
-		keys.insert(keys.begin() + i, key);
-		children.insert(children.begin() + i, child);
-
+		children[key] = child;
 	}
 };
 
@@ -48,11 +33,9 @@ public:
     unordered_map<string, int> duplicates;
 
 	// Create a new node and return a pointer to it.
-	node *new_node(node* parent)
+	node *new_node()
 	{
 		node *n = new node();
-		
-		n->parent = parent;  // TODO: remove this pointer?
 		return n;
 	}
 
@@ -65,16 +48,14 @@ public:
 	// Constructor: initialize an empty tree with just the root.
 	trie()
 	{
-		// The root has one null child pointer.
-		root = new_node(nullptr);
+		root = new_node();
 	}
 
 	// An auxiliary function for deleting a subtree recursively.
 	void delete_trie(node *n)
 	{
-		for (int i = 0; i < n->children.size(); i++)
-			if (n->children[i])
-				delete_trie(n->children[i]);
+		for (auto item : n->children)
+			delete_trie(item.second);
 		delete_node(n);
 	}
 
@@ -86,16 +67,18 @@ public:
 
 	void process_line(string line)
 	{
+
+		// traverse trie by line prefix
 		node *current = root;
-		int position;
 		int line_index = 0;
 		char key = line[line_index];
-		while (line_index < line.length() && current->find_branch(key, position))
+		while (line_index < line.length() && current->has_key(key))
 		{
-			current = current->children[position];
+			current = current->children[key];
 			line_index++;
 			key = line[line_index];
 		}
+
 		if (line_index == line.length())
 		{
 			if (current->children.size() == 0)
@@ -105,26 +88,25 @@ public:
 				return;
 			}
 			// prefix of existing line
-			current->insert_node(0, key, new_node(current));
+			current->insert_node(key, new_node());
 			return;
 		}
 		// unique suffix
-		insert_branch(position, key, line.substr(line_index + 1),current);
+		insert_branch(key, line.substr(line_index + 1), current);		
 	}
 
 private:
 
-	void insert_branch(int i, char key, string suffix, node *current)
+	void insert_branch(char key, string suffix, node *current)
 	{
 		node *last, *next;
-		next = new_node(current);
-		current->insert_node(i, key, next);
+		next = new_node();
+		current->insert_node(key, next);
 		for (char& c : suffix)
 		{
 			last = next;
-			next = new_node(current);
-			last->keys.push_back(c);
-			last->children.push_back(next);
+			next = new_node();
+			last->insert_node(c, next);
 		}
 	}
 };
@@ -134,17 +116,6 @@ private:
 vector<string> find_duplicates(DataGenerator& generator) {
 	/*
 	 * Find duplicates in the given data.
-	 *
-	 * The `generator` provides a forward iterator over strings
-	 * for traversing the data, so it can be iterated for example
-	 * using a `for` cycle:
-	 *
-	 *   for (const string& item : generator) {...}
-	 *
-	 * The `generator` can be traversed multiple times.
-	 *
-	 * The goal is to return a vector of duplicated entries,
-	 * reporting each duplicated entry only once.
 	 */
 
 	trie *t = new trie();
@@ -152,9 +123,10 @@ vector<string> find_duplicates(DataGenerator& generator) {
 	for (const string& line : generator)
 	{
 		t->process_line(line);
+		cout << line << endl;
 	}
 	for (auto d : t->duplicates)
 		duplicates.push_back(d.first);
-
+	t->delete_trie(t->root);
 	return duplicates;
 }
