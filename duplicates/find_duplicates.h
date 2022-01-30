4fc2be3c2bb0a2bce2f817095433f543ef15aba1
LadaKa <LadaKudlackova@seznam.cz>
2022-01-30 12:14:04 +0100

    Moved to separate folder.

---
+++ duplicates/find_duplicates.h
@@ -0,0 +1,144 @@
+#include <bitset>
+#include <unordered_map>
+#include <unordered_set>
+
+/*
+	Data structure that contains hashes of lines
+	in unordered set.
+*/
+class Lines_hashes {
+
+public:
+
+	Lines_hashes()
+	{}
+
+	bool look_up(string line)
+	{
+		int32_t hash_key = get_hash_key(line);
+		return hashes.find(hash_key) != hashes.end();
+	}
+
+	void insert(string line)
+	{
+		int32_t hash_key = get_hash_key(line);
+		hashes.insert(hash_key);
+	}
+
+	bool is_duplicate(string line)
+	{
+		if (look_up(line))
+			return true;
+
+		insert(line);
+		return false;
+	}
+
+private:
+
+	unordered_set<int32_t> hashes;
+
+	int32_t get_hash_key(string line)
+	{
+		hash<string> std_hash;
+		return std_hash(line);
+	}
+};
+
+
+/*
+	Returns whether the hash_key is element of given duplicate_hashes_set
+*/
+bool is_duplicate_hash(unordered_set<int32_t> &duplicate_hashes_set, int32_t hash_key)
+{
+	return ((duplicate_hashes_set.find(hash_key)) != duplicate_hashes_set.end());
+}
+
+
+/*
+	Returns whether there is a line with given hash_key 
+	in hash_lines_map
+*/
+bool has_hash_key(unordered_map<int32_t, string> &hash_lines_map, int32_t hash_key)
+{
+	return (hash_lines_map.find(hash_key) != hash_lines_map.end());
+}
+
+
+/*
+	Returns set of duplicate hashes of lines
+	for given generator
+*/
+unordered_set<int32_t> get_duplicate_hashes_set(DataGenerator& generator)
+{
+	Lines_hashes *lines_hashes = new Lines_hashes();
+	unordered_set<int32_t> duplicate_hashes_set;
+
+	for (const string& line : generator)
+	{
+		hash<string> std_hash;
+		int32_t hash_key = std_hash(line);
+		if (lines_hashes->is_duplicate(line))
+			duplicate_hashes_set.insert(hash_key);
+	}
+
+	delete lines_hashes;
+
+	return duplicate_hashes_set;
+}
+
+/*
+	Returns duplicate lines
+	for given generator
+*/
+vector<string> get_duplicates(DataGenerator& generator, unordered_set<int32_t> duplicate_hashes_set)
+{
+	vector<string> duplicates;
+	unordered_map<int32_t, string> hash_lines_map;
+
+	for (const string& line : generator)
+	{
+		hash<string> std_hash;
+		int32_t hash_key = std_hash(line);
+
+		//	check if line has duplicate hash_key 
+		if (is_duplicate_hash(duplicate_hashes_set, hash_key))
+		{
+			//	check if line with duplicate hash_key 
+			//	is duplicate
+
+			bool is_duplicate_line = false;
+			while ((!is_duplicate_line) && (has_hash_key(hash_lines_map, hash_key)))
+			{
+				//	compare line with the string that has same hash_key
+				if (line == hash_lines_map[hash_key])
+				{
+					// new duplicate found
+					is_duplicate_line = true;
+					duplicates.push_back(line);
+				}
+				//	increase hash_key to handle possible collision
+				hash_key = (hash_key + 1);
+			}
+
+			if (!is_duplicate_line)
+			{
+				//	line is not duplicate, 
+				//	will be inserted to the next free position 
+				//	after strings with the same hash_key
+				hash_lines_map[hash_key] = line;
+			}
+		}
+	}
+	return duplicates;
+}
+
+
+/*
+	Find duplicates in the given data.
+*/
+vector<string> find_duplicates(DataGenerator& generator)
+{
+	unordered_set<int> duplicate_hashes_set = get_duplicate_hashes_set(generator);
+	return get_duplicates(generator, duplicate_hashes_set);
+}
